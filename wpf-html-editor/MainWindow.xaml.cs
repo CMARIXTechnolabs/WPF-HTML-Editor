50c4e74cec898c37c463496279e970423b5af6d4
Happy Patel <happy.cmarix@gmail.com>
2019-01-15 11:50:59 +0530

    Init code

---
+++ wpf-html-editor/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace WPFHtmlEditor
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+
+		#region Constructor
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			//Prepare the html content
+			string html = @"<P><STRONG>Lorem Ipsum is simply dummy text.</STRONG></P>";
+
+			//Bind the html content to Html Editor
+			txtMessage.BindingContent = html;
+		}
+
+		#endregion
+	}
+}
